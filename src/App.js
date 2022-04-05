05dcc8a913bcb59a2b4c341ebec1d4cdbc56e85c
Prakash D <thisisprakashdokka@gmail.com>
2022-04-05 12:26:32 +0000

    added protected routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import {Route, Switch} from 'react-router-dom'
-
-import Footer from './components/Footer'
-import Home from './components/Home'
+import {Route, Switch, Redirect} from 'react-router-dom'
 import Cart from './components/Cart'
+import Home from './components/Home'
 import LoginForm from './components/LoginForm'
 import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
 import './App.css'
 
 /* const sortByOptions = [
@@ -22,11 +21,11 @@ import './App.css'
 */
 const App = () => (
   <Switch>
-    <Route exact path="/" component={Home} />
     <Route exact path="/login" component={LoginForm} />
-    <Route exact path="/cart" component={Cart} />
-    <Route component={NotFound} />
-    <Footer />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/cart" component={Cart} />
+    <Route exact path="/not-found" component={NotFound} />
+    <Redirect to="/not-found" />
   </Switch>
 )
 
