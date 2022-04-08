d940b5c031c75458d35fc040a8f00853507495fc
Prakash D <thisisprakashdokka@gmail.com>
2022-04-08 06:16:45 +0000

    added functionalities

--- src/components/RestaurantCard/index.js
+++ src/components/RestaurantCard/index.js
@@ -10,17 +10,17 @@ const RestaurantCard = props => {
   return (
     <Link
       to={`/restaurant/${id}`}
-      style={{textDecoration: 'none'}}
+      style={{textDecoration: 'none', alignItems: 'center'}}
       testid="restaurant-item"
     >
-      <li className="list-item">
+      <li className="restaurant-card">
         <img src={imageUrl} alt="restaurant" className="restaurant-image" />
-        <div className="restaurant-detail">
+        <div className="restaurant-details">
           <h1 className="restaurant-name">{name}</h1>
           <p className="restaurant-cuisine">{cuisine}</p>
           <div className="ratings-container">
-            <FaStar size="13px" color="#FFCC00" />
-            <p className="rating">{rating}</p>
+            <FaStar size="13px" color="#FFCC00" className="star" />
+            <p className="rest-rating">{rating}</p>
             <p className="reviews">({totalReviews} ratings)</p>
           </div>
         </div>
